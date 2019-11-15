1e7751c24451c288e296d8d7db010f19a61bf9cb
Ajani Queeley <ajaniqueeley@gmail.com>
2019-11-15 17:23:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planet_call)
   planet_call.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planet_call)
+  planet_call.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
